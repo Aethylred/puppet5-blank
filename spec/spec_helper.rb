2c1ec55a1539c685ce5b5be08d801fd72d8bc111
Aaron Hicks <aaron.hicks@niwa.co.nz>
2017-07-11 14:46:31 +1200

    oops, removed some puppet5 variable hashes from spec_helper.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,47 +26,23 @@ RSpec.configure do |c|
     # Declare variable lookup tables for testing here, so they can be reused across tests
     @package_details = {
       'redhat-6-x86_64' => {
-        :version => '5.0.0-1.el6',
-        :package => 'puppet-agent',
+        :version => '12',
+        :package => 'example',
       },
       'redhat-7-x86_64' => {
-        :version => '5.0.0-1.el7',
-        :package => 'puppet-agent',
+        :version => '15',
+        :package => 'example2',
       },
       'centos-6-x86_64' => {
-        :version => '5.0.0-1.el6',
-        :package => 'puppet-agent',
+        :version => '12',
+        :package => 'example4',
       },
       'centos-7-x86_64' => {
-        :version => '5.0.0-1.el7',
-        :package => 'puppet-agent',
+        :version => '15',
+        :package => 'example13',
       },
     }
 
-    @install_directories = {
-      :base          => '/etc/puppetlabs',
-      :code          => '/etc/puppetlabs/code',
-      :mcollective   => '/etc/puppetlabs/mcollective',
-      :puppet        => '/etc/puppetlabs/puppet',
-      :modules       => '/etc/puppetlabs/code/modules',
-      :environments  => '/etc/puppetlabs/code/environments',
-      :ssl           => '/etc/puppetlabs/puppet/ssl',
-      :cert_requests => '/etc/puppetlabs/puppet/ssl/certificate_requests',
-      :certs         => '/etc/puppetlabs/puppet/ssl/certs',
-      :private_certs => '/etc/puppetlabs/puppet/ssl/private',
-      :private_keys  => '/etc/puppetlabs/puppet/ssl/private_keys',
-      :public_keys   => '/etc/puppetlabs/puppet/ssl/public_keys',
-      :pxp_agent     => '/etc/puppetlabs/pxp-agent',
-      :pxp_modules   => '/etc/puppetlabs/pxp-agent/modules'
-    }
-
-    @config_files = {
-      :puppetconf => '/etc/puppetlabs/puppet/puppet.conf'
-    }
-
-    @service_files = {
-      :systemd_service_unit => '/usr/lib/systemd/system/puppet.service'
-    }
   end
 end
 
